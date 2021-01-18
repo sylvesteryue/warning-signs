e5da83a5692e791337c6a22bbdb301948c8a13e7
sylvesteryue <sylvesteryue2301@gmail.com>
2021-01-17 16:46:16 -0800

    modified hand

--- client/src/components/Game/Hand/Hand.js
+++ client/src/components/Game/Hand/Hand.js
@@ -10,17 +10,21 @@ const Hand = (props) => {
     
 
     return(
-        <div style={{display: "flex"}}>
-            <Card key="test1" card_text="testing" type="warning"/>
-            <Card key="test2" card_text="testing again" type="warning"/>
-            <Card key="test3" card_text="warning again" type="warning"/>
-            <Card key="test4" card_text="perk again" type="perk"/>
-            <Card key="test5" card_text="perk" type="perk"/>
-            <Card key="test6" card_text="bruh" type="perk"/>
-            <Card key="test7" card_text="ok" type="perk"/>
+        <div>
+            <p>Select 2 perks and 1 warning card</p>
+            <div style={{display: "flex"}}>
+                <Card key="test1" card_text="testing" type="warning"/>
+                <Card key="test2" card_text="testing again" type="warning"/>
+                <Card key="test3" card_text="warning again" type="warning"/>
+                <Card key="test4" card_text="perk again" type="perk"/>
+                <Card key="test5" card_text="perk" type="perk"/>
+                <Card key="test6" card_text="bruh" type="perk"/>
+                <Card key="test7" card_text="ok" type="perk"/>
 
-            <Button variant="contained" color="primary">Choose</Button>
+                <Button variant="contained" color="primary">Choose</Button>
+            </div>
         </div>
+        
     );
 
 };
